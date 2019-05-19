3caeb19414cc556bc12ffb3cdba04afac0c0404e
fguer2 <fer@DESKTOP-UABLEJK>
2019-05-19 08:15:25 +0200

    cosaschulaaas14

--- cosaschulas/src/cosaschulas/Cosaschulas.java
+++ cosaschulas/src/cosaschulas/Cosaschulas.java
@@ -16,6 +16,7 @@ public class Cosaschulas {
      */
     public static void main(String[] args) {
        System.out.println("todaviamascosaschulaaas");
+       System.out.println("ytodaviamascosaschulaaas");
     }
     
 }
